f7589c4aef86db5e66abe73032cff400c279a849
profdanielly <souza.santos.danielly@escola.pr.gov.br>
2025-08-04 19:11:38 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+const botaoMostraPalavras = document.querySelector(‘#botao-
+palavrachave’);
+
+botaoMostraPalavras.addEventListener
+(‘click’, mostraPalavrasChave);
+
+function mostraPalavrasChave (){
+ ̊  ̊ alert(‘Fui clicado!’);
+}
